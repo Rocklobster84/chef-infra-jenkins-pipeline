eedf249543457dd8bb200a5021bc6f15d7ccc034
Stephanie Thompson <steph@stephcodes,dev>
2022-06-30 08:34:38 -0500

    Added Jenkins file

--- cookbooks/jenkins/recipes/default.rb
+++ cookbooks/jenkins/recipes/default.rb
@@ -8,16 +8,19 @@ apt_update
 
 package 'openjdk-11-jre'
 
-apt_repository "jenkins" do
-  uri "http://pkg.jenkins-ci.org/debian-stable"
-  key "https://pkg.jenkins.io/debian-stable/jenkins.io.key"
-  components ["binary/"]
+apt_repository 'jenkins' do
+  uri 'http://pkg.jenkins.io/debian-stable'
+  key 'https://pkg.jenkins.io/debian-stable/jenkins.io.key'
+  distribution ['binary/']
   action :add
 end
 
-package "jenkins"
+apt_package 'jenkins' do
+  action :install
+end
 
-service "jenkins" do
-  supports [:stop, :start, :restart]
+service 'jenkins' do
   action [:start, :enable]
 end
+
+#sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
