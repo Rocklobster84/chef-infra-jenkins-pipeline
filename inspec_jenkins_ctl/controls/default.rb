f2e5df5bfe83f6e9271ccfec6d1d671c5bd246a3
Stephanie Thompson <thompson@progress.com>
2022-07-14 08:43:03 -0500

    Updated Java control to look for V 11.0.15

---
+++ inspec_jenkins_ctl/controls/default.rb
@@ -0,0 +1,13 @@
+# copyright: 2022, Stephanie Thompson
+
+title 'Jenkins Controller'
+
+# you add controls here
+control 'java' do                                  # A unique ID for this control
+  impact 1.0                                       # The criticality, if this control fails.
+  title 'Java 11 Installation'                     # A human-readable title
+  desc 'Checking that Java 11 is installed'
+  describe 'java' do                               # The actual test
+    its('version') { should cmp >= '11.0.15' }
+  end
+end
